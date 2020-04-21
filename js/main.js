70c468f8ab5b3afeb08beaf70ecfdc23edb67ae6
cryx3001 <chiko3001@gmail.com>
2020-04-21 14:48:50 +0200

    Catching errors

--- js/main.js
+++ js/main.js
@@ -7,78 +7,83 @@ const client = new Discord.Client();
 
 client.login(auth.token);
 
-client.on("ready", () => {
-    console.log(`Logged ! ${client.user.tag}`);
-});
+try {
+    client.on("ready", () => {
+        console.log(`Logged ! ${client.user.tag}`);
+    });
 
 
-client.on("guildCreate", guild =>{
-    let defChannel = "";
-    Array.from(guild.channels.cache.values()).forEach((channel) => {
+    client.on("guildCreate", guild => {
+        let defChannel = "";
+        Array.from(guild.channels.cache.values()).forEach((channel) => {
 
-        if(channel.type === "text" && defChannel === ""){
+            if (channel.type === "text" && defChannel === "") {
 
-            if(channel.permissionsFor(guild.me).has("SEND_MESSAGES")){
-                defChannel = channel;
-                channel.send("Hey! To get started type `sm!help` !");
+                if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+                    defChannel = channel;
+                    channel.send("Hey! To get started type `sm!help` !");
+                }
             }
+        });
+    });
+
+
+    client.on("message", msg => {
+        let sMsg = msg.content.split(' ');
+
+        try {
+            if (msg.content.startsWith("sm!")) {
+                msg.content = msg.content.toLowerCase();
+                console.log(`${Date.now()} ${msg.content}`);
+                switch (sMsg[0]) {
+                    // Basics
+                    case "sm!init":
+                        util.sendMsg(msg, 2, cmd.initializeUser, coolDownSet);
+                        break;
+
+                    case "sm!help":
+                        util.sendMsg(msg, 2, cmd.showHelp, coolDownSet);
+                        break;
+
+                    // trades manipulation
+                    case "sm!newtrade":
+                        util.sendMsg(msg, 2, cmd.newTrade, coolDownSet);
+                        break;
+
+                    case "sm!closetrade":
+                        util.sendMsg(msg, 2, cmd.closeTrade, coolDownSet);
+                        break;
+
+                    case "sm!search":
+                        util.sendMsg(msg, 2, cmd.searchMarket, coolDownSet);
+                        break;
+
+                    case "sm!show":
+                        util.sendMsg(msg, 2, cmd.showMarket, coolDownSet);
+                        break;
+
+                    // Player info
+                    case "sm!balance":
+                        util.sendMsg(msg, 2, cmd.showBalance, coolDownSet);
+                        break;
+
+                    case "sm!list":
+                        util.sendMsg(msg, 2, cmd.showList, coolDownSet);
+                        break;
+
+                    case "sm!daily":
+                        util.sendMsg(msg, 2, cmd.getDaily, coolDownSet);
+                        break;
+                }
+            }
+        } catch (e) {
+            msg.channel.send("Something went terribly wrong! Please send the following text to Cryx#6546\n" +
+                "```\n" + e + "\n```");
+            console.log(e);
         }
     });
-});
-
-
-client.on("message", msg => {
-    let sMsg = msg.content.split(' ');
-
-    try {
-        if(msg.content.startsWith("sm!")){
-            msg.content = msg.content.toLowerCase();
-            switch (sMsg[0]) {
-                // Basics
-                case "sm!init":
-                    util.sendMsg(msg, 2, cmd.initializeUser, coolDownSet);
-                    break;
-
-                case "sm!help":
-                    util.sendMsg(msg, 2, cmd.showHelp, coolDownSet);
-                    break;
-
-                // trades manipulation
-                case "sm!newtrade":
-                    util.sendMsg(msg, 2, cmd.newTrade, coolDownSet);
-                    break;
-
-                case "sm!closetrade":
-                    util.sendMsg(msg, 2, cmd.closeTrade, coolDownSet);
-                    break;
-
-                case "sm!search":
-                    util.sendMsg(msg, 2, cmd.searchMarket, coolDownSet);
-                    break;
-
-                case "sm!show":
-                    util.sendMsg(msg, 2, cmd.showMarket, coolDownSet);
-                    break;
-
-                // Player info
-                case "sm!balance":
-                    util.sendMsg(msg, 2, cmd.showBalance, coolDownSet);
-                    break;
-
-                case "sm!list":
-                    util.sendMsg(msg, 2, cmd.showList, coolDownSet);
-                    break;
-
-                case "sm!daily":
-                    util.sendMsg(msg, 2, cmd.getDaily, coolDownSet);
-                    break;
-            }
-        }
-    }
-    catch (e) {
-        msg.channel.send("Something went terribly wrong! Please send the following text to Cryx#6546\n" +
-            "```\n"+e+"\n```");
-        console.error(e);
-    }
-});
+}
+catch (e) {
+    console.log(e);
+}
 
